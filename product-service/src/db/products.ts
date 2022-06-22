fedf1ebddcb9e970f6219382cd6e8fc2165b21a4
lizaveta56k <lizaveta.kaliaha@gmail.com>
2022-06-22 22:37:34 +0200

    fixed issue with getById

--- product-service/src/db/products.ts
+++ product-service/src/db/products.ts
@@ -17,7 +17,7 @@ export const getById = async (id) => {
     const { rows } = await
         query(`select p.id, p.title, p.price, p.description, s.count from products p inner join stocks s on s.product_id=p.id where p.id = '${id}'`);
 
-    return rows;
+    return rows[0];
 }
 
 export const create = async (title, description, price) => {
