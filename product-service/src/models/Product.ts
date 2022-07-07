ba1539a8bb3f7d2de96c651f7bb259c05ae212c4
lizaveta56k <lizaveta.kaliaha@gmail.com>
2022-07-07 22:54:32 +0200

    added product valdation

--- product-service/src/models/Product.ts
+++ product-service/src/models/Product.ts
@@ -13,4 +13,5 @@ export const ProductSchema = Yup.object().shape({
   title: Yup.string().required(),
   description: Yup.string(),
   price: Yup.number().required(),
+  count: Yup.number().required(),
 });
